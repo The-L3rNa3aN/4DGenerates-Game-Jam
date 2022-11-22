f02cb9ab608fab3a8ac428ccd95773186fbd01a9
Veman Jadhav <vemancraft@gmail.com>
2022-11-22 19:31:47 +0530

    Added a countdown timer and a game timer.
    A "321" countdown timer that executes at the start of the game and a game timer with a functioning "game over" event.
    
    Next up: a grading system.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,19 @@ public class GameManager : MonoBehaviour
     public int cartNum;
     public int levelSelected;
 
+    [Header("Player Score")]
+    public int score = 0;
+
+    [Header("Time")]
+    public float gameTimer;         //Measured in seconds.
+    public bool isTimerRunning;
+
+    [Header("On Start Countdown")]
+    public GameObject countdownPanel;
+    public GameObject num3;
+    public GameObject num2;
+    public GameObject num1;
+
     [Header("Pause Menu")]
     public GameObject pauseMenu;
     public bool isPaused;
@@ -14,9 +27,6 @@ public class GameManager : MonoBehaviour
     [Header("Other References")]
     private GameObject cameraManager;
     [SerializeField] private GameObject[] cartAgents;
-
-    [Header("Player Score")]
-    public int score = 0;
 
     private void Start()
     {
@@ -28,8 +38,37 @@ public class GameManager : MonoBehaviour
 
         cameraManager = FindObjectOfType<CameraManager>().gameObject;
         levelSelected = PlayerPrefs.GetInt("levelSelectValue");
+
+        switch(levelSelected)                           //Set gameTimer based on the difficulty chosen.
+        {
+            case 1:
+                gameTimer = 240f;
+                break;
+
+            case 2:
+                gameTimer = 300f;
+                break;
+
+            case 3:
+                gameTimer = 360f;
+                break;
+        }
+
+        StartCoroutine(CoBeginCountdown());             //Start a "3-2-1" countdown for the player to prepare.
     }
 
+    private void Update()
+    {
+        if (isTimerRunning) RunTimer();
+
+        if(gameTimer <= 0f)
+        {
+            isTimerRunning = false;
+            Debug.Log("Time's up!");
+        }
+    }
+
+    #region Key Presses: 1, 2, 3, Esc
     private void Alpha1Selected() => cartNum = 1;
 
     private void Alpha2Selected()
@@ -69,6 +108,40 @@ public class GameManager : MonoBehaviour
             pauseMenu.SetActive(false);
         }
     }
+    #endregion
 
     public void AddScore(int add) => score += add;
+
+    private void RunTimer() => gameTimer -= Time.deltaTime;
+
+    private IEnumerator CoBeginCountdown()
+    {
+        foreach(GameObject cart in cartAgents)                      //Disable interactions such as the camera and the carts until the countdown is over.
+            cart.GetComponent<CartAgent>().enabled = false;
+        cameraManager.GetComponent<CameraManager>().enabled = false;
+
+        countdownPanel.SetActive(true);
+        num3.SetActive(true);
+        Debug.Log("3...");
+
+        yield return new WaitForSeconds(1f);
+        num3.SetActive(false);
+        num2.SetActive(true);
+        Debug.Log("2...");
+
+        yield return new WaitForSeconds(1f);
+        num2.SetActive(false);
+        num1.SetActive(true);
+        Debug.Log("1...");
+        
+        yield return new WaitForSeconds(1f);
+        num1.SetActive(false);
+        countdownPanel.SetActive(false);
+
+        foreach (GameObject cart in cartAgents)                     //Enable the camera and carts now that the countdown is over.
+            cart.GetComponent<CartAgent>().enabled = true;
+        cameraManager.GetComponent<CameraManager>().enabled = true;
+
+        isTimerRunning = true;                                      //Start the game timer.
+    }
 }
