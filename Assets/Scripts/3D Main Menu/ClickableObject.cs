dee56cf9943f7fefe2ed8bc5b57678ff9bd3be46
Veman Jadhav <vemancraft@gmail.com>
2022-11-29 23:12:22 +0530

    Updated 123.unity, and added MainMenu_3D.unity and ClickableObject.cs
    Updated the orientation of CameraHolderB in 123.unity to be similar to that of CameraHolderA.
    
    Added a new scene for creating a 3D main menu and an accompanying script which detects any mouse clicks on the object it's attached to.

---
+++ Assets/Scripts/3D Main Menu/ClickableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+
+    //MAKE A FUNCTION SELECTOR.
+
+    private void OnMouseDown()
+    {
+        Debug.Log("test");
+    }
+}
