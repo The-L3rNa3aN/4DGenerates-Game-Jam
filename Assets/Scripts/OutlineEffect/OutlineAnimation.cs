4e0ed8b6afdcecb9accd2b77e5a6611c90ac43f3
Veman Jadhav <vemancraft@gmail.com>
2022-12-05 22:34:57 +0530

    Added code for outlining.
    Not my code. Code belongs to one Jose Guerreiro.

---
+++ Assets/Scripts/OutlineEffect/OutlineAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+namespace cakeslice
+{
+    public class OutlineAnimation : MonoBehaviour
+    {
+        private bool pingPong = false;
+
+        private void Update()
+        {
+            Color c = GetComponent<OutlineEffect>().lineColor0;
+
+            if(pingPong)
+            {
+                c.a += Time.deltaTime;
+
+                if(c.a >= 1)
+                    pingPong = false;
+            }
+            else
+            {
+                c.a -= Time.deltaTime;
+
+                if(c.a <= 0)
+                    pingPong = true;
+            }
+
+            c.a = Mathf.Clamp01(c.a);
+            GetComponent<OutlineEffect>().lineColor0 = c;
+            GetComponent<OutlineEffect>().UpdateMaterialsPublicProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/OutlineEffect/OutlineManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace cakeslice
+{
+    public class OutlineManager : MonoBehaviour
+    {
+        public Outline[] Outlines;
+
+        private void Start() => Outlines = transform.GetComponentsInChildren<Outline>();
+
+        private void OnEnable() => SetEnable();
+
+        private void OnDisable() => SetDisable();
+
+
+        public void SetEnable() {
+            foreach (Outline o in Outlines)
+            {
+                o.enabled = true;
+            }
+        }
+
+        public void SetDisable() {
+            foreach (Outline o in Outlines)
+            {
+                o.enabled = false;
+            }
+        }
+    }
+}
