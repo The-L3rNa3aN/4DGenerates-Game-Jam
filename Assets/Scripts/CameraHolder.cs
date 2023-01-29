65f97e240f544cd665ac01dbeec1f86c530c040c
Veman Jadhav <vemancraft@gmail.com>
2023-01-29 16:55:15 +0530

    3D text in 123.unity and new lighting.
    Also, removed CameraHolder.cs and commented out the Start code in CameraManager.cs

--- Assets/Scripts/CameraHolder.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class CameraHolder : MonoBehaviour
-{
-    public Transform target;
-
-    private void Start() => transform.LookAt(target);
-}
--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -15,9 +15,9 @@ public class CameraManager : MonoBehaviour
 
     private void Start()
     {
-        transform.SetParent(posA);
+        /*transform.SetParent(posA);
         transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localRotation = Quaternion.Euler(Vector3.zero);*/
         InputManager.tabInput += ChangeCamPosition;
     }
 
